699302d50d9a02646271be9507b6bae76108bda0
ets-gopi <gopi.m@entitledarts.com>
2025-05-21 12:00:20 +0000

    added tickets srv

--- tickets/src/app.ts
+++ tickets/src/app.ts
@@ -2,7 +2,7 @@ import express from 'express';
 import 'express-async-errors';
 import { json } from 'body-parser';
 import cookieSession from 'cookie-session';
-import { errorHandler, NotFoundError } from '@rallycoding/common';
+import { errorHandler, NotFoundError } from '@gdtickets.com/common';
 
 const app = express();
 app.set('trust proxy', true);
